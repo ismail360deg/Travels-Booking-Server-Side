48aa149eca809a1cac11fd03e827b72ceec7c05c
ismail360deg <ismailbhuiyan2000@gmail.com>
2023-02-16 16:11:25 +0600

    'await'

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions = {
 mongoose.set('strictQuery', false);
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,{
+         mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
